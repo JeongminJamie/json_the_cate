aafdf51a2fe29e960ef41dadd17ecdcff5962cfc
JeongminJamie <sksrhcalsu@gmail.com>
2022-05-12 18:14:34 -0400

    breed fetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,34 @@
-const arg = process.argv.slice(2);
+// const arg = process.argv.slice(2);
 const request = require('request');
 
-const siberian = function () {
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${arg}`, function (error, response, body) {
-    const data = JSON.parse(body);
-    console.log(data[0].description);
+// const breedDescription = function () {
+//   request(`https://api.thecatapi.com/v1/breeds/search?q=${arg}`, function (error, response, body) {
+//     const data = JSON.parse(body);
+//     console.log(data[0].description);
 
-    if (arg === undefined) {
-      return "type in your breed";
-    }
+//     if (arg === undefined) {
+//       return "type in your breed";
+//     }
+//     if (error) {
+//       return error;
+//     }
+//   })
+// }
+const fetchBreedDescription = function (breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`
+  request(url, function (error, response, body) {
     if (error) {
-      return error;
+      callback(error)
+      return;
+    }
+    const data = JSON.parse(body);
+    if (data[0] === undefined) {
+      callback("breed is not found");
+    } else {
+      callback(null, data[0].description)
     }
   })
-}
-siberian();
+
+};
+
+module.exports = { fetchBreedDescription };
